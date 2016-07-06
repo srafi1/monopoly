a660c8319dab31a515592c00062086e2a4d3e7b7
srafi1 <srafi1@stuy.edu>
2016-07-05 23:15:22 -0400

    almost everything done

--- cgi-bin/monopoly.py
+++ cgi-bin/monopoly.py
@@ -1,6 +1,53 @@
 #! /usr/bin/python
+import cgi,cgitb
+cgitb.enable()
+form = cgi.FieldStorage()
 
 print "Content-type: text/html\n\n"
-print "<html><head><title>Monopoly</title></head><body>"
 
-print "</body></html>"
+def main() :
+	if len(form) == 0:
+		print '<html><head><title>Nah man</title></head><body><h1>No players, no game</h1></body></html>'
+		return
+	print '''
+	<html><head>
+	<title>Monopoly</title>
+	<script type=text/javascript src=../monopoly.js></script>
+	<style type=text/css>
+	body {
+	font-family: Arial;
+	}
+	td, button {
+	font-size:24;
+	border:4px solid black;
+	}
+	</style>
+	</head><body onload=setup('''+ str(len(form)) +''')>
+	<center>
+	<h1>All da munnies</h1><br>
+	<table style="border-collapse:collapse;width:80%"><tr>
+	'''
+	
+	players = len(form)
+	p = form.keys()
+	for i in range(players):
+		p[i] = form[p[i]].value
+	if players > 4:
+		for i in range(4):
+			print '<td id=',i,'>'+ p[i] +':<br>$1500<br><button onclick=pay(',i,')>Pay</button></td>'
+		print '</tr><tr><td colspan=4><center><table style="border-collapse:collapse"><tr>'
+		for i in range(players - 4):
+			print '<td id=',i+4,'>'+ p[i+4] +':<br>$1500<br><button onclick=pay(',i+4,')>Pay</button></td>'
+		print '</tr></table></center></td></tr>'
+	else:
+		for i in range(players):
+			print '<td id=',i,'>'+ p[i] +':<br>$1500<br><button onclick=pay(',i,')>Pay</button></td>'
+		print '</tr>'
+	print '</table><br><table style="border-collapse:collapse;width:50%"><tr><td id=-1 style="text-align:center">BANK:<br>$999999<br><button onclick=pay(-1)>Pay</button></td></tr></table>'
+	
+	for i in range(players):
+		print '<p id=p'+str(i),'hidden>'+ p[i] +'</p>'
+	
+	print "</center></body></html>"
+
+main()
---
+++ monopoly.js
@@ -0,0 +1,47 @@
+var players = [];
+var tformat = 'name:<br>$money<br><button onclick=btnfxn(index)>btnfxn</button>';
+var payer = '';
+var paid = '';
+
+function setup(n) {
+	
+	for (var i = 0; i < n; i++) {
+		var p = document.getElementById('p'+i).innerHTML;
+		players[i] = {
+			name: p,
+			money: 1500
+		}
+	}
+	render('pay');
+}
+
+function render(newbtn) {
+	for (var i = 0; i < players.length; i++) {
+		var rep = tformat.replace('name', players[i].name).replace('index', i).replace('money', players[i].money).replace('btnfxn',newbtn).replace('btnfxn',newbtn);
+		document.getElementById(i).innerHTML = rep;
+	}
+	var rep = tformat.replace('name', 'BANK').replace('index', -1).replace('$money', 'ALL DA MUNNIES').replace('btnfxn',newbtn).replace('btnfxn',newbtn);
+	document.getElementById(-1).innerHTML = rep;
+}
+
+function pay(player) {
+	payer = player;
+	render('receive');
+}
+
+function receive(player) {
+	paid = player;
+	var amt = parseInt('0' + prompt('How much money?'));
+	if (payer == -1 || paid == -1) {
+		if (paid == -1 && payer == -1) {
+			render('pay');
+			return;
+		} else if (payer != -1) players[payer].money -= amt; 
+		else if (paid != -1) players[paid].money += amt;
+	} else {
+		alert(players[payer].name + ' paid ' + players[paid].name + ' $' + amt);
+		players[payer].money -= amt;
+		players[paid].money += amt;
+	}
+	render('pay');
+}
